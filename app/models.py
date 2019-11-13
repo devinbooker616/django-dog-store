b33e057523c495532fd89ac36eb18e34772443ec
Base Camp <basecamp@ssa-406.local>
2019-11-13 09:54:25 -0600

    created 'DogProduct' model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class DogProduct(models.Model):
+    name = models.TextField()
+    product_type = models.TextField()
+    dog_size = models.TextField()
+    price = models.FloatField()
+    quantity = models.IntegerField()
