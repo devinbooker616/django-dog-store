072e0806d486582ebdbc5341fda9de2105a0b1a1
Base Camp <basecamp@ssa-406.local>
2019-11-13 14:53:23 -0600

    added missing url

--- dog_store/urls.py
+++ dog_store/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", app.views.home, name="home"),
     path("dog-product/<dog_product_id>", app.views.dog_product_detail, name="dog_product_detail"),
+    path("dog-product/<dog_product_id>/purchase",app.views.purchase_dog_product, name="purchase_dog_product"),
     path("purchase/<purchase_id>", app.views.purchase_detail, name="purchase_detail"),
     path("dogtag/new", app.views.new_dog_tag, name="new_dog_tag"),
     path("dogtag", app.views.dog_tag_list, name="dog_tag_list")
