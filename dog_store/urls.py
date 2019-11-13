17cc6dd14a2fc73c0b3c2cffc5d5fd22e601c9b0
Base Camp <basecamp@ssa-406.local>
2019-11-13 09:51:58 -0600

    added all required urls

--- dog_store/urls.py
+++ dog_store/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import app.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", app.views.home, name="home"),
+    path("dog-product/<dog_product_id>", app.views.dog_product_detail, name="dog_product_detail"),
+    path("purchase/<purchase_id>", app.views.purchase_detail, name="purchase_detail"),
+    path("dogtag/new", app.views.new_dog_tag, name="new_dog_tag"),
+    path("dogtag", app.views.dog_tag_list, name="dog_tag_list")
 ]
